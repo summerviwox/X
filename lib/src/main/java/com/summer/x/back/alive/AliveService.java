d2e70381d88ebf621f8a1d6e0a71de4f6e7e4b48
summerviwox <summerviwox@gmail.com>
2019-05-13 15:40:46 +0800

    包活处理

--- lib/src/main/java/com/summer/x/back/alive/AliveService.java
+++ lib/src/main/java/com/summer/x/back/alive/AliveService.java
@@ -2,7 +2,10 @@ package com.summer.x.back.alive;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
+
+import com.summer.x.R;
 
 import androidx.annotation.Nullable;
 
@@ -11,5 +14,23 @@ public class AliveService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alive);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.start();
+            }
+        });
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mediaPlayer.start();
+            }
+        });
+        return super.onStartCommand(intent, flags, startId);
     }
 }
