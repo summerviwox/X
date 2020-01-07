1c4db9ed11fc2bb9759e618f6446935d94a520dc
summerviwox <summerviwox@gmail.com>
2020-01-07 10:00:03 +0800

    compile 'com.ashokvarma.android:bottom-navigation-bar:2.1.0'

--- app/src/main/java/com/summer/app/MainActivity.java
+++ app/src/main/java/com/summer/app/MainActivity.java
@@ -6,15 +6,24 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.summer.x.base.ui.XActivity;
 
-public class MainActivity extends Activity {
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
     }
 
+
+    @OnClick({R.id.root})
+    public void onClick(View v) {
+        ToastUtils.showShort("dfdf");
+    }
 }
